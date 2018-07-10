e1d88ef1efbeaaa73df83862b1d8776b6ea4c96c
leocavalcante <lc@leocavalcante.com>
2018-07-10 19:17:53 -0300

    Add delete method

--- lib/graphapi.dart
+++ lib/graphapi.dart
@@ -14,12 +14,22 @@ class GraphAPI {
 
   FutureOr<Map<String, dynamic>> post(
       String objectId, String edge, Map<String, dynamic> node) async {
-    final url =
-        '$_endpoint/$_version/$objectId/$edge?access_token=$_accessToken';
+    final url = _buildUrl(objectId, edge);
 
     final response =
         await http.post(url, headers: _headers, body: json.encode(node));
 
     return new Map.from(json.decode(response.body));
   }
+
+  FutureOr<bool> delete(String objectId) async {
+    final url = _buildUrl(objectId);
+    final response = await http.delete(url);
+    final Map<String, bool> result = json.decode(response.body);
+
+    return result['success'];
+  }
+
+  String _buildUrl(String objectId, [String edge = '']) =>
+      '$_endpoint/$_version/$objectId/$edge?access_token=$_accessToken';
 }
